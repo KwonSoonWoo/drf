4b631c8bd0babde50ef78fab287e27a356fcee0c
ksw <zenuine1@gmail.com>
2018-07-19 12:01:23 +0900

    작성한 snippet_list, snippet_detail을 views와 urls에서 django_view모듈로 분리

---
+++ app/snippets/urls/__init__.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+
+# /snippets/snippets/
+# /snippets/snippets/<pk>/
+
+# /snippets/django_view/snippets/
+# /snippets/django_view/snippets/<pk>
+
+from . import django_view
+
+app_name = 'snippets'
+urlpatterns = [
+    # config.urls를 참조
+    path('django_view/', include(django_view)),
+]
